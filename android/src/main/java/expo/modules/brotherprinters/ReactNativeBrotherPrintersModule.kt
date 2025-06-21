65a118fc937ed938778b08c6e3ccfc6b6df8ff38
Nikita <nikita@usekojo.com>
2025-06-21 11:07:29 -0300

    Update ReactNativeBrotherPrintersModule.kt

--- android/src/main/java/expo/modules/brotherprinters/ReactNativeBrotherPrintersModule.kt
+++ android/src/main/java/expo/modules/brotherprinters/ReactNativeBrotherPrintersModule.kt
@@ -34,41 +34,10 @@ class ReactNativeBrotherPrintersModule : Module() {
     // The module will be accessible from `requireNativeModule('ReactNativeBrotherPrinters')` in JavaScript.
     Name("ReactNativeBrotherPrinters")
 
-//    // Sets constant properties on the module. Can take a dictionary or a closure that returns a dictionary.
-//    Constants(
-//      "PI" to Math.PI
-//    )
-//
-//    // Defines event names that the module can send to JavaScript.
-//    Events("onChange")
-//
-//    // Defines a JavaScript synchronous function that runs the native code on the JavaScript thread.
-//    Function("hello") {
-//      "Hello world! 👋"
-//    }
-//
-//    // Defines a JavaScript function that always returns a Promise and whose native code
-//    // is by default dispatched on the different thread than the JavaScript runtime runs on.
-//    AsyncFunction("setValueAsync") { value: String ->
-//      // Send an event to JavaScript.
-//      sendEvent("onChange", mapOf(
-//        "value" to value
-//      ))
-//    }
-//
-//    // Enables the module to be used as a native view. Definition components that are accepted as part of
-//    // the view definition: Prop, Events.
-//    View(ReactNativeBrotherPrintersView::class) {
-//      // Defines a setter for the `name` prop.
-//      Prop("name") { view: ReactNativeBrotherPrintersView, prop: String ->
-//        println(prop)
-//      }
-//    }
-
-
     Function("discover") {
       // https://support.brother.com/g/s/es/htmldoc/mobilesdk/guide/discover-printer.html
-      return@Function PrinterSearcher.startUSBSearch(context).channels
+      // return@Function PrinterSearcher.startUSBSearch(context).channels
+      Log.d("", "Success - Print Image")
     }
 
     Function("printImage") { base64image: String ->
