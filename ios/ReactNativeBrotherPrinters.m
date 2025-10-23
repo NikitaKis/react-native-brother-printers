bd37c916ea1f1b398499439cc826d435d2b9a794
Nikita Kiselev <nikitkiselev@gmail.com>
2025-10-23 11:24:22 -0300

    better errors 2

--- ios/ReactNativeBrotherPrinters.m
+++ ios/ReactNativeBrotherPrinters.m
@@ -142,7 +142,7 @@ RCT_REMAP_METHOD(printImage, deviceInfo:(NSDictionary *)device printerUri: (NSSt
     if (driverGenerateResult.error.code != BRLMOpenChannelErrorCodeNoError ||
         driverGenerateResult.driver == nil) {
         NSLog(@"%@", @(driverGenerateResult.error.code));
-        reject(PRINT_ERROR, @"There was an error trying to print the image", Nil);
+        reject(@"ERROR_CODE", @(driverGenerateResult.error.code).description, driverGenerateResult.error);
         return;
     }
 
